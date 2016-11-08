bb37b9896df1e0a725f9129f514b12527184fecb
Dmitry Ponyatov <dponyatov@gmail.com>
2016-11-08 18:09:05 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ hpp.hpp
@@ -0,0 +1,33 @@
+#ifndef _H_HPP
+#define _H_HPP
+
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Sym {
+	string val;
+	Sym(string);
+	vector<Sym*> nest; void push(Sym*);
+	virtual string head(); string pad(int); virtual string dump(int=0);
+};
+
+struct Num:Sym { Num(string); };
+struct Str:Sym { Str(string); };
+
+struct Op:Sym { Op(string); };
+
+struct Vector:Sym { Vector(); string head(); };
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+extern int yyparse();
+extern void yyerror(string);
+#include "ypp.tab.hpp"
+
+#endif // _H_HPP
